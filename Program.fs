078454d5fb66997b5163ee3e9e13c103a9253cf0
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-04-02 18:09:26 +0300

    Lab5 Task6

--- Program.fs
+++ Program.fs
@@ -1,17 +1,26 @@
 ﻿let sumOfDigits n=
     let rec sumDigits n curSum =
-        if n = 0 then curSum
-         else
+        match n with
+         | 0 -> curSum
+         | _ ->
              let n1 = n/10
              let cifr = n%10
-             let newSum = curSum + cifr
-             sumDigits n1 newSum
+             sumDigits n1 (curSum + cifr)
     sumDigits n 0
 
-let main () =
-    printf "Введите число: "
-    let number = System.Console.ReadLine() |> int
-    let sum = sumOfDigits number
-    System.Console.WriteLine(sum)
+let factorial n=
+    let rec mul n curMul =
+        match n with
+        | 0 | 1 -> curMul
+        | _ -> mul(n-1) (curMul*n)
+    mul n 1
 
-main ()
+let isLogicalFunc logicVar numericVar =
+    match logicVar with
+    | true -> sumOfDigits numericVar
+    | false -> factorial numericVar
+
+let main () =
+        System.Console.WriteLine(isLogicalFunc false 12)
+        System.Console.WriteLine(isLogicalFunc true 12)
+main()
