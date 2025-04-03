9841632ae2bbf80793825967e25beef4985c5ab1
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-04-03 04:43:47 +0300

    Lab5 Task9 reduceBy

--- Program.fs
+++ Program.fs
@@ -1,19 +1,32 @@
-﻿let rec reduce (number:int) (func:int->int->int) (initNumber:int) =
-    match number with
-    | 0 -> initNumber
+﻿let rec reduceBy (number:int) (func:int->int->int) (initialAcc:int) (predicate:int->bool)=
+    match abs(number) with
+    | 0 -> initialAcc
     | _ ->
-        let digit  = number % 10
-        let newInitNumber = func initNumber digit
-        reduce (number/10) func newInitNumber
+        let digit  = abs(number) % 10
+        let newAcc =
+            match predicate digit with
+            | true -> func initialAcc digit
+            | false -> initialAcc
+        reduceBy (abs(number)/10) func newAcc predicate
+
+let isPrime (number:int)=
+    let rec divisor (num:int) (div:int)=
+        match div*div>num, num%div=0 with
+        | true, _ -> true
+        | _, true -> false
+        | _ -> divisor num (div+1)
+    match number with
+    | _ when number<=1 -> false
+    | 2 -> true
+    | _ when number%2=0 -> false
+    | _  -> divisor number 3
+    
+    
 
 let main ()=
     
-    System.Console.WriteLine("Минимальное {0}",reduce 123 (fun a b -> match a with | _ when a<=b->a | _ -> b) 10)
-    System.Console.WriteLine("Произведение {0}",reduce 123 (fun a b->a*b)  1)
-    System.Console.WriteLine("Количество чисел {0}", reduce 123 (fun _ a->a+1) 0)
+    System.Console.WriteLine("Максимальное нечетное: {0}",reduceBy 123456789 (fun a b -> match a with | _ when a>=b->a | _ -> b) -1 (fun d -> d%2=1))
+    System.Console.WriteLine("Произведение простых: {0}",reduceBy 123456789 (*) 1 (fun d -> isPrime d=true))
+    System.Console.WriteLine("Количество чисел кратных 5: {0}", reduceBy 123456789 (fun _ a -> a+1) 0 (fun d -> d%5=0))
 
 main()
-
-
-
-    
