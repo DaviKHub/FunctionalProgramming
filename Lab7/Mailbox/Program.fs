6145e4f624da0845dd884e6b928f4857a783bd5b
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-04-17 05:36:55 +0300

    Lab7 Task3,4

---
+++ Lab7/Mailbox/Program.fs
@@ -0,0 +1,32 @@
+module MailBox.Program
+open System
+
+type Message =
+    | PrintMessage of string    
+    | DoubleNumber of int       
+    | Stop                      
+
+let agent = MailboxProcessor.Start(fun inbox ->
+    let rec loop () =
+        async {
+            let! msg = inbox.Receive()
+
+            match msg with
+            | PrintMessage text ->
+                printfn "Получено сообщение: %s" text
+            | DoubleNumber num ->
+                printfn "Число удвоено: %d" (num * 2)
+            | Stop ->
+                printfn "Агент завершает работу."
+                return ()
+            return! loop()
+        }
+    loop ()
+)
+
+agent.Post (PrintMessage "Hello, world!")
+agent.Post (DoubleNumber 131)                 
+agent.Post (DoubleNumber -1)               
+agent.Post Stop
+
+Console.ReadLine()
---
+++ Lab7/Parse/Program.fs
@@ -0,0 +1,38 @@
+module FParsec.Program
+
+open FParsec
+type Expr =
+    | Num of int
+    | Add of Expr * Expr
+    | Sub of Expr * Expr
+
+let numParser: Parser<Expr, unit> =
+    pint32 |>> fun n -> Num n
+
+let addParser: Parser<Expr, unit> =
+    (spaces >>. numParser .>> spaces) .>>. (pchar '+' .>> spaces) .>>. (spaces >>. numParser)
+    |>> fun ((left, _), right) -> Add (left, right)
+
+let subParser: Parser<Expr, unit> =
+    (spaces >>. numParser .>> spaces) .>>. (pchar '-' .>> spaces) .>>. (spaces >>. numParser)
+    |>> fun ((left, _), right) -> Sub (left, right)
+
+let exprParser: Parser<Expr, unit> =
+    addParser <|> subParser <|> numParser
+
+let parseExpression (input: string) =
+    match run exprParser input with
+    | Success(result, _, _) -> Some result
+    | Failure(msg, _, _) -> None
+
+let input1 = "3 + 4"
+let input2 = "10 - 3"
+let input3 = "5"
+
+let result1 = parseExpression input1
+let result2 = parseExpression input2
+let result3 = parseExpression input3
+
+printfn "Result 1: %A" result1
+printfn "Result 2: %A" result2
+printfn "Result 3: %A" result3
